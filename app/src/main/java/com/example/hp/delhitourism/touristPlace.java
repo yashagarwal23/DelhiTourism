3a16e15b5c8a9167728bb335ae1ff4f9206a5794
Paras Sindhwani <parassindhwani2010@gmail.com>
2019-01-02 10:34:33 +0530

    second commit

---
+++ app/src/main/java/com/example/hp/delhitourism/touristPlace.java
@@ -0,0 +1,24 @@
+package com.example.hp.delhitourism;
+
+import java.net.URL;
+
+public class touristPlace {
+     String name;
+     String id;
+     URL imageURL;
+     int starRating;
+     String category;
+     String location;
+     String description;
+     String about;
+     touristPlace()
+     {
+         name="";
+         id="";
+         starRating=0;
+         category="";
+         location="";
+         description="";
+         about="";
+     }
+}
